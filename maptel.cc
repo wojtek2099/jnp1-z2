b8338f3d5c7087d1740b22617b048f72bdc4a966
wojtek2099 <pawelolejnik.pap@gmail.com>
2021-10-28 17:57:19 +0200

    poprawki stylistyczne

--- maptel.cc
+++ maptel.cc
@@ -8,83 +8,83 @@ using namespace std;
 
 namespace {
 
-    unordered_map<unsigned long, unordered_map<string, string> >& dicts() {
-        static unordered_map<unsigned long, unordered_map<string, string> > dicts;
-        return dicts;
+unordered_map<unsigned long, unordered_map<string, string>>& dicts() {
+    static unordered_map<unsigned long, unordered_map<string, string>> dicts;
+    return dicts;
+}
+
+bool has_loop(unsigned long id, char const *tel_src) {
+    unordered_map<string, string>& dict = ::dicts().at(id);
+
+    string a(tel_src);
+    string b = a;
+    if (dict.count(a) == 0) {
+        return false;
     }
 
-    bool has_loop(unsigned long id, char const *tel_src) {
-        unordered_map<string, string>& dict = ::dicts().at(id);
+    while (true) {
+        a = dict.at(a);
+        if (dict.count(a) == 0) return false;
 
-        string a(tel_src);
-        string b = a;
-        if (dict.count(a) == 0) {
-            return false;
-        }
+        b = dict.at(b);
+        if (dict.count(b) == 0) return false;
+        b = dict.at(b);
+        if (dict.count(b) == 0) return false;
 
-        while (true) {
-            a = dict.at(a);
-            if (dict.count(a) == 0) return false;
-
-            b = dict.at(b);
-            if (dict.count(b) == 0) return false;
-            b = dict.at(b);
-            if (dict.count(b) == 0) return false;
-
-            if (a == b) return true;
-        }   
-    }
+        if (a == b) return true;
+    }   
+}
 
 }
 
 namespace jnp1 {
 
-    unsigned long maptel_create(void) {
-        static unsigned long max_id = 0;
-        ::dicts().insert(make_pair(max_id, unordered_map<string, string>()));
-        return max_id++;
-    }
+unsigned long maptel_create(void) {
+    static unsigned long max_id = 0;
+    ::dicts().insert(make_pair(max_id, unordered_map<string, string>()));
+    return max_id++;
+}
 
-    void maptel_delete(unsigned long id) {
-        assert(::dicts().count(id)!=0);
-        ::dicts().erase(id);
-    }
+void maptel_delete(unsigned long id) {
+    assert(::dicts().count(id)!=0);
+    ::dicts().erase(id);
+}
 
-    void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
-        ::dicts().at(id).insert_or_assign(string(tel_src), string(tel_dst));
-    }
+void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
+    ::dicts().at(id).insert_or_assign(string(tel_src), string(tel_dst));
+}
 
-    void maptel_erase(unsigned long id, char const *tel_src) {
-        ::dicts().at(id).erase(string(tel_src));
-    }
+void maptel_erase(unsigned long id, char const *tel_src) {
+    ::dicts().at(id).erase(string(tel_src));
+}
 
-    void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
-        try {
-            unordered_map<string, string>& dict = ::dicts().at(id);
+void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
+    try {
+        unordered_map<string, string>& dict = ::dicts().at(id);
 
-            if (dict.count(string(tel_src)) == 0) {
-                memcpy(tel_dst, tel_src, min(len, jnp1::TEL_NUM_MAX_LEN+1));
-                return;
-            }
-
-            string tmp_src(tel_src);
-            string tmp_dst(dict.at(tmp_src));
-
-            if (::has_loop(id, tel_src)) {
-                strncpy(tel_dst, tel_src, len);
-                return;
-            }
-
-            while (dict.count(tmp_dst) != 0) {
-                tmp_src = tmp_dst;
-                tmp_dst = dict.at(tmp_src);
-            }
-
-            strncpy(tel_dst, tmp_dst.c_str(), len);
-
-        } catch (out_of_range& oor) {
-            cerr << "OOR error: " << oor.what() << endl;
+        if (dict.count(string(tel_src)) == 0) {
+            memcpy(tel_dst, tel_src, min(len, jnp1::TEL_NUM_MAX_LEN+1));
+            return;
         }
+
+        string tmp_src(tel_src);
+        string tmp_dst(dict.at(tmp_src));
+
+        if (::has_loop(id, tel_src)) {
+            strncpy(tel_dst, tel_src, len);
+            return;
+        }
+
+        while (dict.count(tmp_dst) != 0) {
+            tmp_src = tmp_dst;
+            tmp_dst = dict.at(tmp_src);
+        }
+
+        strncpy(tel_dst, tmp_dst.c_str(), len);
+
+    } catch (out_of_range& oor) {
+        cerr << "OOR error: " << oor.what() << endl;
     }
+}
 
 }
